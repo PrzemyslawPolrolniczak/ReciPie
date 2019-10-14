e357bb018dde799b7bcf7896551e3f209209a9bc
PrzemyslawPolrolniczak <przemekp96@gmail.com>
2019-10-14 23:27:37 +0200

    feat: add sqlite3 database

---
+++ server/models/recipe.js
@@ -0,0 +1,25 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Recipe = sequelize.define(
+    "Recipe",
+    {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      ingredients: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      direction: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      }
+    },
+    {}
+  );
+  Recipe.associate = function(models) {
+    Recipe.BelongsTo(models.User, { foreignKey: "userId" });
+  };
+  return Recipe;
+};
